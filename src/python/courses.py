dffe5e1bf7e01fd37232c2e4b19e80631b2bf93e
Andre Genes <andre.genes.vieira@gmail.com>
2020-01-11 14:12:13 +0200

    Created dictonary of courses and magic skillz

---
+++ src/python/courses.py
@@ -0,0 +1,7 @@
+courses = {
+    1 : 'Alchemy basics',
+    2 : 'Alchemy advanced',
+    3 : 'Magic for day-to-day life',
+    4 : 'Magic for medical professionals',
+    5 : 'Dating with magic',
+}
