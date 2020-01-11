615121cd8c8279da68ba0e802d35e6d0203b1efe
Andre Genes <andre.genes.vieira@gmail.com>
2020-01-11 23:15:37 +0200

    Created folders of react and started navbar

---
+++ src/python/students.py
@@ -0,0 +1,10 @@
+students = {
+    std1 : {
+        'first_name' : '',
+        'last_name' : '',
+        'creation_time' : '',
+        'update_time' : '',
+        'magic_skillz' : [],
+        'desired_skillz' : [],
+    }
+}
