3476f3c8a2ec8ba1c3901df74dbaa84f222b3d84
Andre Genes <andre.genes.vieira@gmail.com>
2020-01-13 12:14:50 +0200

    backend logic finished

--- Back_End/server-hogward.py
+++ Back_End/server-hogward.py
@@ -21,17 +21,24 @@ def student_list():
     elif request.method == 'POST':
         return add_student()
 
+
 def student_list():
     return jsonify(students=list(student_data.list_of_students().values()))
+
 
 @app.route("/students/<student_id>", methods=['GET', 'DELETE', 'PUT'])
 def student_handler(student_id):
     if request.method == 'GET':
-        return jsonify(student_data.individual_student(student_id))
+        return get_individual_student(student_id)
     elif request.method == 'DELETE':
-        return jsonify(student_data.remove(student_id))
+        return remove_student(student_id)
     elif request.method == 'PUT':
         return update_student()
+
+def remove_student(student_id):
+    student = get_individual_student(student_id)
+    student_data.remove_student(student_id)
+    return student
 
 
 # @app.route("/dashboard")
@@ -39,22 +46,25 @@ def student_handler(student_id):
 #     dashboard_display = request.get('')
 #     return dashboard_display
 
+def get_individual_student(student_id):
+    return jsonify(student_data.individual_student(student_id))
+
 
 def add_student():
     data = request.get_json()
     student = Student(data['first_name'], data['last_name'], data['interest_course'], data['magic_skillz'],
                       data['desired_skillz'])
     student_data.create_student(student)
-    return jsonify(student_data.individual_student(Student.student_id))
+    return get_individual_student(student.student_id)
 
 
 def update_student():
     data = request.get_json()
-    student = Student(first_name=data['first_name'], last_name=data['last_name'],
+    student = Student(first_name=data['first_name'], last_name=data['last_name'], student_id=data['student_id'],
                       interest_course=data['interest_course'],
-                      magic_skillz=data['magic_skillz'], desired_skillz=data['desired_skillz'])
+                      magic_skillz=data['magic_skillz'], desired_skillz=data['desired_skillz'], creation_time=data['creation_time'])
     student_data.update_student(student)
-    return jsonify(student_data.individual_student(Student.student_id))
+    return get_individual_student(student.student_id)
 
 
 if __name__ == '__main__':
--- Back_End/students.py
+++ Back_End/students.py
@@ -9,13 +9,13 @@ class Student:
         if student_id is None:
             self.student_id = str(Student.counter)
             Student.counter += 1
-            self.creation_time = time.gmtime(time.time())
+            self.creation_time = time.asctime(time.gmtime(time.time()))
             self.update_time = self.creation_time
 
         else:
             self.student_id = student_id
             self.creation_time = creation_time
-            self.update_time = time.gmtime(time.time())
+            self.update_time = time.asctime(time.gmtime(time.time()))
         self.first_name = first_name
         self.last_name = last_name
         self.magic_skillz = magic_skillz
@@ -26,9 +26,9 @@ class Student:
 class StudentsData:
     def __init__(self):
         self.students = {
-            '1': Student('Harry', 'Potter', {'Magic day to day life'}, {'Conjuror': 2}, {'Conjuror': 3},),
-            '2': Student('Hermione', 'Granger', {'Magic day to day life'}, {'Alchemy': 4}, {'Alchemy': 5},),
-            '3': Student('Ron', 'Weisly', {'Magic day to day life'}, {'Animation': 1}, {'Animation': 2},)
+            '1': Student('Harry', 'Potter', ['Magic day to day life', 'Date a Witch'], {'Conjuror': 2}, {'Conjuror': 3},),
+            '2': Student('Hermione', 'Granger', ['Magic day to day life', 'Date a Witch'], {'Alchemy': 4}, {'Alchemy': 5},),
+            '3': Student('Ron', 'Weisly', ['Magic day to day life', 'Date a Witch'], {'Animation': 1}, {'Animation': 2},)
         }
 
     def list_of_students(self):
